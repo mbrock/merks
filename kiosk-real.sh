f05a3b37b170d21da6ba2ee88fd71093daf85f26
Mikael Brockman <mikael@brockman.se>
2019-10-11 15:21:16 +0300

    use mbrock home

--- kiosk-real.sh
+++ kiosk-real.sh
@@ -13,7 +13,7 @@ xrandr --output DP-1 --mode 1920x1080
 
 echo ok
 sleep 2
-chromium-browser /home/kiosk/merks/kiosk.html --window-size=1920x1080 --window-position=0,0 \
+chromium-browser /home/mbrock/merks/kiosk.html --window-size=1920x1080 --window-position=0,0 \
   --kiosk \
   --incognito --noerrdialogs --disable-translate \
   --disable-pinch \
